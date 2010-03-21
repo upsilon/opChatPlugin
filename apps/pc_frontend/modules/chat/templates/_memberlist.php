da3e45bca84a18610b366130f3384aa29bab8bb9
Shogo Kawahara <ooharabucyou@gmail.com>
2010-03-21 22:54:37 +0900

    メンバーリストのリンクでポップアップするようにする

--- apps/pc_frontend/modules/chat/templates/_memberlist.php
+++ apps/pc_frontend/modules/chat/templates/_memberlist.php
@@ -1,5 +1,5 @@
 <?php foreach($memberlist as $member): ?>
 <dt>
-<?php echo link_to($member->Member->getName(), '@obj_member_profile?id='.$member->Member->id); ?>
+<?php echo link_to($member->Member->getName(), '@obj_member_profile?id='.$member->Member->id, array('popup' => true)); ?>
 </dt>
 <?php endforeach; ?>
