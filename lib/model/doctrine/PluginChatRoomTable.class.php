7152b565b11848248fdfa76f3ecb2760140f28bd
upsilon <kim.upsilon@gmail.com>
2010-03-24 08:21:31 +0900

    チャットルーム一覧を最終書込日時の降順にする

--- lib/model/doctrine/PluginChatRoomTable.class.php
+++ lib/model/doctrine/PluginChatRoomTable.class.php
@@ -11,8 +11,10 @@ class PluginChatRoomTable extends Doctrine_Table
 {
   public function getListPager($page = 1, $size = 20)
   {
-    $query = $this->createQuery()
-      ->orderBy('updated_at DESC');
+    $query = $this->createQuery('r')
+      ->leftJoin('r.ChatContent c')
+      ->groupBy('r.id')
+      ->orderBy('MAX(c.created_at) DESC');
    
     $pager = new sfDoctrinePager('ChatRoom', $size);
     $pager->setQuery($query);
